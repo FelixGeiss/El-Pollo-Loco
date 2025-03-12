1d18eb7faa946758a418bdb8961f8e83e58d0b73
Felix <geissq@gmail.com>
2025-03-12 14:28:56 +0100

    Removed unnecessary images.

--- js/models/salsaStore/salsa_store.class.js
+++ js/models/salsaStore/salsa_store.class.js
@@ -5,7 +5,7 @@ class SalsaStore extends DrawableObject {
   x = 200;
 
   constructor() {
-    super().loadImage("img/6_salsa_bottle/salsa_store.png");
+    super().loadImage("img/salsa_store.png");
     this.offset.top = 25;
     this.offset.bottom = 35;
     this.x = 500 + Math.random() * (10000 - 500);
