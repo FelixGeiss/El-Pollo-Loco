cbdcb6b6cbdc39ad2e91d7978a14f5ce28481f78
Felix <geissq@gmail.com>
2025-03-17 16:40:35 +0100

    bug fix

--- js/models/icons/play-game.class.js
+++ js/models/icons/play-game.class.js
@@ -92,5 +92,10 @@ class PlayGameIcon extends DrawableObject {
     this.world.startGame = true;
     this.world.startAllIntervals();
     this.world.showStartscreen = false;
+    if (!this.world.audioManager.backgroundSound.muted) {
+      this.world.audioManager.backgroundSound.play();
+    } else {
+      this.world.audioManager.backgroundSound.muted = false;
+    }
   }
 }
